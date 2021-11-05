17701896a163eeef1ce7ba5b1b04c0175720a380
LiaoYuFu.Leo <378693857@gmail.com>
2021-11-05 15:42:32 -0400

    try_

--- try_erlang.erl
+++ try_erlang.erl
@@ -1,5 +1,16 @@
 -module(try_erlang).
--export([start/0]).
+-export([start/0,while/1,while/2]).
+
 
 start() ->
-    io:fwrite("hello, erlang\n").
+    Arr = [1,3,5,7,9],
+    % while(Arr).
+    Element = Arr/0,
+    io:fwrite(Element).
+
+while(Arr) -> while(Arr,0).
+while([],Index) -> Index;
+while([Element|Arr],Index) -> 
+    io:fwrite("~w\t~w\n",[Index,Element]),
+    while(Arr,Index+1).
+    
