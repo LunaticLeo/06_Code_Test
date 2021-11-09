7ffc8ce7d39242dcf8ba20ee0cf3840e6b7c639e
liaoyufu-leo <liaoyufu.leo@gmail.com>
2021-11-08 19:54:32 -0500

    11082021

--- try.js
+++ try.js
@@ -1,10 +1,26 @@
-transaction = {
-    "transaction_id": ObjectId(),
-    "item_id": ObjectId(),
-    "buyer_id": ObjectId(),
-    "date": "11/02/2021 21:30:30",
-    "type": "credit card",
-    "rate": 5,
-    "status": "completed",
-    "value": 600
+function check(input, datatype) {
+    try {
+        switch (datatype) {
+            case "account":
+                if (input == undefined) throw datatype + " should not be none";
+                if (typeof (input) != "string") throw datatype + " should be string";
+                if (input.length == 0) throw datatype + " should not be empty";
+                input = input.trim();
+                if (input.length == 0) throw datatype + " should not be all space";
+                if (! /^[a-zA-Z0-9]{5,}@stevens.edu*/.test(input)) throw datatype + "not valid";
+                return true;
+            default:
+                throw "should provide a check datatype for input";
+        }
+    } catch (error) {
+        return false;
+    }
 }
+
+function main() {
+    let errors = [], account = "";
+    account = check(account, "account") ? account.trim() : errors.push();
+    console.log(errors, account)
+}
+
+main();
--- try_erlang.erl
+++ try_erlang.erl
@@ -1,16 +1,32 @@
 -module(try_erlang).
--export([start/0,while/1,while/2]).
+%% -export([start/0,while/1,while/2]).
+-compile(export_all).
+-include_lib("./try_erlang.hrl").
 
+start()->
+    #ship{id=1,name="yufuliao"}.
 
-start() ->
-    Arr = [1,3,5,7,9],
-    % while(Arr).
-    Element = Arr/0,
-    io:fwrite(Element).
+show(Input)->
+    io:fwrite("~w",[Input#ship.id]).
 
 while(Arr) -> while(Arr,0).
 while([],Index) -> Index;
 while([Element|Arr],Index) -> 
     io:fwrite("~w\t~w\n",[Index,Element]),
     while(Arr,Index+1).
+
+%% eb 7 exercise 5
+mult(Num,Num2) ->
+    Num*Num2.
+
+double(Num) ->
+    Num*2.
+
+distance(Tuple,Tuple2) ->
+    {X , Y} = Tuple,
+    {X2 , Y2 } = Tuple2,
+    DisX = X2 - X,
+    DisY = Y2 - Y,
+    %math:sqrt(DisX * DisX + DisY*DisY).
+    DisX * DisX + DisY*DisY.
     
