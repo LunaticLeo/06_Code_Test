1d5cce63f544cae2e04f2e203322a4d1d6531270
liaoyufu-leo <liaoyufu.leo@gmail.com>
2022-09-19 13:27:51 -0400

    20220919

--- try.bat
+++ try.bat
@@ -1,6 +1,9 @@
 @echo on
 title Sync floders to github
-echo welcome
-cd
-git checkout
+set message=%date:~6,4%%date:~0,2%%date:~3,2%
+cd ../Graduate && git add . && git commit . -m %message% && git push
+cd ../Master && git add . && git commit . -m %message% && git push
+cd ../Jobs && git add . && git commit . -m %message% && git push
+cd ../Code_Try && git add . && git commit . -m %message% && git push
+cd ../Algorithm && git add . && git commit . -m %message% && git push
 pause
