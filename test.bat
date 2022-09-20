62f6b96c68d2a208b6b9a78d20c2a27d62b46a1a
liaoyufu-leo <liaoyufu.leo@gmail.com>
2022-09-20 00:38:17 -0400

    20220920

--- test.bat
+++ test.bat
@@ -1,20 +1,20 @@
 @echo off
 title Sync floders to github
 set message=%date:~6,4%%date:~0,2%%date:~3,2%
-echo 'Sync Gradute Folder'
+echo '\nSync Gradute Folder' 
 cd ../Graduate && git add . && git commit . -m %message% && git push
 
-echo 'Sync Master Folder'
+echo '\nSync Master Folder'
 cd ../Master && git add . && git commit . -m %message% && git push
 
-echo 'Sync Jobs Folder'
+echo '\nSync Jobs Folder'
 cd ../Jobs && git add . && git commit . -m %message% && git push
 
-echo 'Sync Code_Test Folder'
+echo '\nSync Code_Test Folder'
 cd ../Code_Test && git add . && git commit . -m %message% && git push
 
-echo 'Sync Algorithm Folder'
+echo '\nSync Algorithm Folder'
 cd ../Algorithm && git add . && git commit . -m %message% && git push
 
-echo "Sync complete"
+echo "\nSync complete"
 pause
