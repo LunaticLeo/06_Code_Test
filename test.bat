3f84c0e65faaecff5c6204f71b3a35ef80057134
liaoyufu-leo <liaoyufu.leo@gmail.com>
2023-01-18 17:39:08 -0500

    01/18/2023

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 @echo off
 title Sync floders to github
-set message=%date:~10,4%
+set message=%date:~4,10%
 echo %message%
 echo. && echo 'Sync Gradute Folder' 
 cd ../Graduate && git commit -am %message% && git push
