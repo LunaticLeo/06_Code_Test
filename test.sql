8613f64dd60a9c75652b51a0cc1b8d01c7fb6a21
liaoyufu-leo <liaoyufu.leo@gmail.com>
2023-03-23 23:53:27 -0400

    sync

--- test.sql
+++ test.sql
@@ -16,14 +16,43 @@
 --     from city_count_country
 -- )
 
-with constituency_max as (
-    select constituency_id, max(votes) as max
-    from results
-    group by constituency_id
+-- with constituency_max as (
+--     select constituency_id, max(votes) as max
+--     from results
+--     group by constituency_id
+-- )
+-- select candidates.party, count(results.constituency_id)
+-- from results, candidates, constituency_max
+-- where constituency_max.constituency_id = results.constituency_id
+-- and constituency_max.max = results.votes
+-- and candidates.id = results.candidate_id
+-- group by candidates.party
+
+
+with t as (
+    select readings.account_id, readings.amount as amount, tariffs.name as tariff_name, tariffs.cost, readings.amount * tariffs.cost as price
+    from tariffs, readings
+    where readings.tariff_id = tariffs.id
+),
+t2 as (
+    select account_id, max(cost) as max_cost, sum(amount) as consumption, sum(price) as total_cost
+    from t
+    group by account_id
+    
+),
+t3 as (
+    select t.account_id, t.tariff_name as highest_tariff, t2.consumption, t2.total_cost
+    from t, t2
+    where t.account_id = t2.account_id and 
+    t.cost = t2.max_cost
 )
-select candidates.party, count(results.constituency_id)
-from results, candidates, constituency_max
-where constituency_max.constituency_id = results.constituency_id
-and constituency_max.max = results.votes
-and candidates.id = results.candidate_id
-group by candidates.party
+select accounts.username, accounts.email, t3.highest_tariff, t3.consumption, round(t3.total_cost,2)
+from t3, accounts
+where t3.account_id = accounts.id
+order by accounts.username
+
+
+
+
+
+
--- test_java_eclipse/src/test_java_eclipse/test.java
+++ test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,6 +1,7 @@
 package test_java_eclipse;
 
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class test {
 
@@ -10,13 +11,37 @@ public class test {
 
 	public class Solution {
 
+		public static int validQuadruples(int N, int A[]) {
+	        
+	        //this is default OUTPUT. You can change it.
+	        int result= 0;
+	        
+	        //write your Logic here:
+	        int[] nums = new int[4];
+	        
+	        for(int i=0;i<N;i++) {
+	        	nums[i%4] = A[i];
+	        	
+	        	if( i>=4 && (nums[i%4]+nums[(i+1)%4]) ==  (nums[(i+2)%4]+nums[(i+3)%4]) ) {
+	        		result ++;
+	        	}
+	        }
+	        
+	        return result ==0 ? -404 : result;
+	    }
+		
 		public static void main(String[] args) throws Exception {
-			HashSet<Integer> set = new HashSet<Integer>();
-			
-			
-			
-			
-			return;
+			Scanner sc = new Scanner(System.in);
+	        int N = sc.nextInt();
+	        
+	        int A [] = new int[N];
+	        for(int i=0; i<N; i++) {
+	            A[i] = sc.nextInt();
+	        }
+	        
+	        // OUTPUT [uncomment & modify if required]
+	        System.out.print(validQuadruples(N,A));
+	        sc.close();
 		}
 	}
 }
