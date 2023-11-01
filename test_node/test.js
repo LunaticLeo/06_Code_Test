4c4a7429576e4f6d6a3d73cb4341f6faa76a7b99
liaoyufu-leo <liaoyufu.leo@gmail.com>
2023-11-01 02:10:07 -0400

    sync

--- test_node/test.js
+++ test_node/test.js
@@ -1,18 +1,23 @@
 // 汉字 [\u4e00-\u9fa5]
 
-const axios = require("axios");
+const fs = require('fs');
 
 async function main() {
-    let str = "";
-    for (let i = 0; i < 100; i++) {
-        let { data } = await axios.get("https://0ijq1i6sp1.execute-api.us-east-1.amazonaws.com/dev/stream");
+	const filePath = 'test.txt'; // 你需要替换成你的文件路径
+	const fileContent = fs.readFileSync(filePath, 'utf-8');
 
-        // console.log(data)
+	// 使用正则表达式以数字+“章”为分隔符进行分割
+	const chapters = fileContent.split(/[0-9]+章/).filter(chapter => chapter.trim() !== '');
+	// 计算每个章节的长度
+	for (let i = 0; i < chapters.length; i++) {
+		const chapterText = chapters[i].trim();
+		const chapterLength = chapterText.length;
 
-        str += data;
-    }
-    console.log(str.length);
-    console.log(str);
+		if (chapterLength < 1000) {
+			console.log(`第${i + 1}章长度: ${chapterLength}字符`);
+			console.log(chapterText);
+		}
+	}
 }
 
 main();
