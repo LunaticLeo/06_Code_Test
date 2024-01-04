05407c7a90a441f70591a7dbe3df7ea7aff8c307
liaoyufu-leo <liaoyufu.leo@gmail.com>
2024-01-04 12:54:31 -0500

    sync

--- test_java_eclipse/src/test_java_eclipse/test.java
+++ test_java_eclipse/src/test_java_eclipse/test.java
@@ -2,6 +2,7 @@ package test_java_eclipse;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
@@ -13,11 +14,18 @@ public class test {
 		System.out.println(Arrays.stream(arr).max());
 	}
 
-	public class Solution {
-
-		public static void main(String[] args) throws Exception {
-			
-			
+	public static String solution(String s) {
+		HashMap<Character, Boolean >hm = new HashMap<Character, Boolean>();
+		
+		StringBuffer output = new StringBuffer();
+		
+		for(int i = 0; i<s.length();i++) {
+			if(hm.getOrDefault(s.charAt(i), false) == false) {
+				hm.put(s.charAt(i),true);
+				output.append(s.charAt(i));
+			}
 		}
+		
+		return output.toString();
 	}
 }
