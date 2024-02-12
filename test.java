71266f41c654a522e5f0d82b993e278d548c0dc4
liaoyufu-leo <liaoyufu.leo@gmail.com>
2024-02-12 13:05:19 -0500

    sync

--- test.java
+++ test.java
@@ -9,9 +9,9 @@ public class test
     public static void main( String[] args )
     {   
     
-        int[] arr = new int[7](4,5,62,1,3,56,7);
+        int[] arr = new int[]{4,5,62,1,3,56,7};
 
-        sort(arr);
+        sort(arr, 0, 7);
 
         for(int i : arr){
             System.out.println(i);
@@ -19,14 +19,14 @@ public class test
 
     }
 
-    void sort(int[] arr, int a, int b){
+    static void sort(int[] arr, int a, int b){
         if(a==b){
             return ;
         }
 
         int point = a+1;
         for(int i=a+1;i<b;i++){
-            if(arr[i]<arr[a] && point != i){
+            if(arr[i]<arr[a]){
                 int temp = arr[i];
                 arr[i] = arr[point];
                 arr[point] = temp;
@@ -34,12 +34,8 @@ public class test
             }   
         }
         if(arr[point] > arr[a]){
-            int temp = arr[point-1];
-            arr[point-1] = arr[a];
-            arr[a] = temp;
-            point = point -1;
-        }
-        if(arr[point] < arr[a]){
+            point = point - 1;
+        }else{
             int temp = arr[point];
             arr[point] = arr[a];
             arr[a] = temp;
@@ -47,6 +43,6 @@ public class test
         sort(arr, a, point);
         sort(arr, point+1, b);
     }
-
+}
 
  
