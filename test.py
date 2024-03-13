b178350268d998156aff58a1de5458ce0692f1fe
liaoyufu-leo <liaoyufu.leo@gmail.com>
2024-03-13 16:43:11 -0400

    sync

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
-a = b = 0
+def decode(message_file):
+    with open(message_file, 'r') as file:
+        lines = file.readlines()
 
-print(a, b)
+    data = []
+
+    for line in lines:
+        parts = line.split(' ')
+        data.append((int(parts[0]),parts[1]))
+    
+    data.sort()
+
+    step = 1
+    sum = 0
+    output = ""
+    while sum< len(data):
+        output += str(data[sum + step - 1][0]) + ': ' + data[sum + step - 1][1]
+        sum += step
+        step +=1
+
+    return output
+
+file_path = './test.txt'
+output = decode(file_path)
+print(output)
