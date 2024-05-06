d5cbc6244d47cc75df25f05e5a6fd5b7172d0f1a
liaoyufu-leo <liaoyufu.leo@gmail.com>
2024-05-05 23:30:46 -0400

    sync

--- test.sql
+++ test.sql
@@ -1,4 +1,28 @@
-select *
-from t1, t2
-where t1.pk = t2.pk and 
-(t1.column1 != t1.column1 or ...)
+with num as (
+    select author, num as count(author), sum as sum((total_number_of_pages-pages_read)/speed)
+    from book_library
+    group by author
+),
+days as (
+    select id, author, book, (total_number_of_pages-pages_read)/speed
+    from book_library 
+)
+
+select
+from num, book_library, days
+where num.author = book_library.author and book_library = days.id and num.author = days.author
+order by num.num DESC, sum, (book_library.total_number_of_pages - book_library.pages_read)/speed, book_library.book
+
+
+
+with num as (
+    select author, num as count(author), sum as sum((total_number_of_pages-pages_read)/speed)
+    from book_library
+    group by author
+)
+
+select
+from num, book_library
+where num.author = book_library.author 
+order by num.num DESC, sum, (book_library.total_number_of_pages - book_library.pages_read)/speed, book_library.book
+
