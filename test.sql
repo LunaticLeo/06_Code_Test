a0932995aff054de2d2039f35225451ccf381f71
liaoyufu-leo <liaoyufu.leo@gmail.com>
2024-06-01 22:58:26 -0400

    sync

--- test.sql
+++ test.sql
@@ -1,28 +1,21 @@
-with num as (
-    select author, num as count(author), sum as sum((total_number_of_pages-pages_read)/speed)
-    from book_library
-    group by author
+with need_practice as(
+    select practice_id
+    from practices
+    WHERE 
+        dateadded >= UNIX_TIMESTAMP('2012-01-01 00:00:00') 
+        AND dateadded <  UNIX_TIMESTAMP('2012-12-31 11:59:59');
 ),
-days as (
-    select id, author, book, (total_number_of_pages-pages_read)/speed
-    from book_library 
+order_status as(
+    select practice_id, 
+        CASE 
+            WHEN product_line is "implants" or product_line is "crowns" or product_line is "bridges" THEN 'fixed'
+            ELSE "removables" 
+        END as category  
+    from order
+    where practice_id in need_practice
 )
 
-select
-from num, book_library, days
-where num.author = book_library.author and book_library = days.id and num.author = days.author
-order by num.num DESC, sum, (book_library.total_number_of_pages - book_library.pages_read)/speed, book_library.book
-
-
-
-with num as (
-    select author, num as count(author), sum as sum((total_number_of_pages-pages_read)/speed)
-    from book_library
-    group by author
-)
-
-select
-from num, book_library
-where num.author = book_library.author 
-order by num.num DESC, sum, (book_library.total_number_of_pages - book_library.pages_read)/speed, book_library.book
-
+select practice_id, 
+    count(case when  order_status.category = "fixed" then 1) as ""
+from need_practice left join order_status on need_practice.practice_id = order_status.practice_id
+group by need_practice.practice_id, order_status.category
