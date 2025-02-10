52a7ea450f1bc5f36eeae94257ce2523ec3f473e
LunaticLeo <liaoyufu.leo@gmail.com>
2025-02-10 01:33:37 -0500

    sync

--- test_java_eclipse/src/test_java_eclipse/test.java
+++ test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,38 +1,14 @@
 package test_java_eclipse;
 
+import java.lang.Boolean;
 import java.util.*;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
-
 
 public class test {
 
 	public static void main(String[] args) throws Exception {
-		String str = "Welcome to Pax.";
-
-        String[] a = str.split(" ");
-
-        int i =0, j = a.length -1;        
-
-        while(i<j){
-            String temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;  
-            i++;
-            j--;
-        }
-
-        String output = "";
-
-        for(i= 0;i<a.length;i++){
-            output = output.concat(a[i] + " ");
-        }
-
-        System.out.println(output);
-
+		byte a = 90;  
+		a = (byte)(a*2); //所以这一行会报错，提示 Type mismatch: cannot convert from int to byte
+		System.out.print(a);
 	}
 
 }
