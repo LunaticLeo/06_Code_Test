f538585679ab62d89e6e4bd9089195b58e55a57c
LunaticLeo <liaoyufu.leo@gmail.com>
2025-02-20 00:35:49 -0500

    sync

--- test_java_eclipse/src/test_java_eclipse/test.java
+++ test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,21 +1,11 @@
 package test_java_eclipse;
 
-import java.lang.Boolean;
-import java.util.*;
+
 
 public class test {
 
 	public static void main(String[] args) throws Exception {
-//		double a = 3.1415926f;
-//		System.out.printf("%f",a); // 3.141593, 
-//		System.out.printf("%10.1f*",a); //00000003.1*, 右对齐，并且以0填充空白
-//		System.out.printf("%010.1f*",a); //        3.1*, 右对齐输出前有空白
-//		System.out.printf("%-10.1fa",a); // 3.141593, 左对齐，输出完之后有空白
-//		System.out.printf("%-010.1fa",a); // 报错，左对齐不能与0填充一起使用
-		
-		int b =1;
-		System.out.printf("%10.2d",b);
-		
+		StringBuffer sb = new StringBuffer();
 
 	}
 
