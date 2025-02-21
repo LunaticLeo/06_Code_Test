40057d570666c4eab037bbf1d7057cc9347af69d
LunaticLeo <liaoyufu.leo@gmail.com>
2025-02-21 00:08:13 -0500

    sync

--- test_java_eclipse/src/test_java_eclipse/test.java
+++ test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,12 +1,23 @@
 package test_java_eclipse;
 
-
-
 public class test {
 
-	public static void main(String[] args) throws Exception {
-		StringBuffer sb = new StringBuffer();
+	public static void main(String[] args) throws InterruptedException {
 
-	}
+        StringBuffer stringBuffer = new StringBuffer();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < 100; i++) {
+            new Thread(new Runnable() {
+                public void run() {
+                	stringBuilder.append(1);
+                	stringBuffer.append(1);
+                }
+            }).start();
+        }
+        Thread.sleep(100);
+        System.out.println("stringBuilder:" + stringBuilder.length() + " stringBuffer:" + stringBuffer.length());
+
+    }
 
 }
