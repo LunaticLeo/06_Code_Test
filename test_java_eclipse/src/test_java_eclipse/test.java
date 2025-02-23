33bc14822bc25b248e892ac611c084343917ef68
LunaticLeo <liaoyufu.leo@gmail.com>
2025-02-23 17:52:20 -0500

    sync

--- test_java_eclipse/src/test_java_eclipse/test.java
+++ test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,22 +1,14 @@
 package test_java_eclipse;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
+
 public class test {
 
 	public static void main(String[] args) throws InterruptedException {
 
-        StringBuffer stringBuffer = new StringBuffer();
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i < 100; i++) {
-            new Thread(new Runnable() {
-                public void run() {
-                	stringBuilder.append(1);
-                	stringBuffer.append(1);
-                }
-            }).start();
-        }
-        Thread.sleep(100);
-        System.out.println("stringBuilder:" + stringBuilder.length() + " stringBuffer:" + stringBuffer.length());
+		HashMap hm = new HashMap<Integer, Integer>();
 
     }
 
