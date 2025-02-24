85742fc7dc7836279c123102b46c53fbbe9df7cd
LunaticLeo <liaoyufu.leo@gmail.com>
2025-02-24 00:48:18 -0500

    sync

--- test_java_eclipse/src/test_java_eclipse/test.java
+++ test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,15 +1,21 @@
 package test_java_eclipse;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.List;
 
 public class test {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		HashMap hm = new HashMap<Integer, Integer>();
+		int[] arr = { 1, 11, 3, 2 };
+		
+		Integer[] list = Arrays.stream(new int[]{1,2,3}).boxed().toArray(Integer[]::new);
 
-    }
+		Arrays.sort(arr);
+		
+		System.out.print(arr.clone() == arr);
+
+	}
 
 }
