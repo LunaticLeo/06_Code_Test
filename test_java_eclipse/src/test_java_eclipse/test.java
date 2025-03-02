8446e621a19abea056d6375c058470dea77ba969
LunaticLeo <liaoyufu.leo@gmail.com>
2025-03-01 20:21:24 -0500

    sync

--- test_java_eclipse/src/test_java_eclipse/test.java
+++ test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,19 +1,57 @@
 package test_java_eclipse;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Comparator;
+import java.io.IOException;
 
-public class test {
+@Deprecated // Use your custom annotation here
+public class test<T extends Number & Comparable<T>> extends Number implements Comparable<test<T>> {
 
-	public static void main(String[] args) throws InterruptedException {
+    // Fields
+    private T x;
+    public final String name;
+    
+    // Constructor
+    public test(int x, String name) {
+        this.x = (T) Integer.valueOf(x);  // Cast to T (assuming T is a subtype of Number like Integer)
+        this.name = name;
+    }
+    
+    // Methods
+    public void myMethod() throws IOException {
+        if (x.doubleValue() < 0) {
+            throw new IOException("Negative number");
+        }
+        System.out.println("Method executed with x = " + x);
+    }
 
-		ArrayList<String> al = new ArrayList<>(); // Raw type, no generics
-        
-        al.add("1");
-        
-        System.out.print(al.add("2"));
+    public static void staticMethod() {
+        System.out.println("This is a static method");
+    }
 
-	}
+    public void defaultMethod() {
+        System.out.println("This is a default method");
+    }
 
+    // imolement Comparable interface
+    public int compareTo(test<T> other) {
+        return x.compareTo(other.x);  // Comparing the 'x' field of the class
+    }
+
+    @Override 
+    public int intValue() {
+        return x.intValue();  // Implementation of abstract method from Number class
+    }
+    
+    // Inner class implementing method from Comparable
+    public class InnerClass implements Comparable<test<T>> {
+        @Override
+        public int compareTo(test<T> o) {
+            return x.compareTo(o.x);
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println("test");
+    }
 }
