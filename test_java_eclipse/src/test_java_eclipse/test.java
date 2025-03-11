b3fb29475f6200d500ea246d788e18fe770b6bbb
LunaticLeo <liaoyufu.leo@gmail.com>
2025-03-11 00:00:15 -0400

    sync

--- test_java_eclipse/src/test_java_eclipse/test.java
+++ test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,20 +1,8 @@
 package test_java_eclipse;
 public class test {
-	//  error: non-static variable this cannot be referenced from a static context
-//    class test1{
-//        public static int i = 1;
-//    }
-    
-    // static inner class
-	static class test2 { 
-		public static int i = 0;
-	}
+
 	public static void main(String[] args) {
-		class test3 {
-			public static int i = 1;
-		}
-		test2 t2 = new test2();
-		test3 t3 = new test3();
-		System.out.println(t2.i + " " + t3.i);
+		
+		System.out.println(Math.round(1));
 	}
 }
