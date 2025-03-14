3fd3667f94b21fd0aa8bd92d44b9787fb4b2b10c
LunaticLeo <liaoyufu.leo@gmail.com>
2025-03-14 00:10:49 -0400

    sync

--- test_java_eclipse/src/test_java_eclipse/test.java
+++ test_java_eclipse/src/test_java_eclipse/test.java
@@ -1,9 +1,11 @@
 package test_java_eclipse;
 
 public class test {
-	public static void main(String[] args) {
-		
-		double r = () -> Math.random();
-		
-	}
+    public static void main(String[] args) {
+    	
+    	Object a = "aaa";
+    	System.out.println(a instanceof Object);
+        System.out.println(new Object() instanceof Integer);
+        // Incompatible conditional operand types String and ArrayList
+    }
 }
